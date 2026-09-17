6ef16e71ed55dd3bb0dc51e2405cebd20ac244b5
Diego <diegoseptiembre991@gmail.com>
2026-09-17 09:49:03 -0500

    Actualización: los autores fueron anonimizados

--- check_robots.py
+++ check_robots.py
@@ -14,9 +14,13 @@ def chekear_robots(url_robot, url_target):
 print("EC:")
 chekear_robots("https://www.elespectador.com/robots.txt", "https://www.elespectador.com/buscador/migraci%C3%B3n-venezolana")
 print("MM:")
-chekear_robots("https://www.milenio.com/robots.txt", "https://www.milenio.com/buscador?text=migrantes+venezolanos")
+chekear_robots("https://www.milenio.com/robots.txt", "https://www.milenio.com/buscador?")
 print("HM:")
 chekear_robots("https://heraldodemexico.com.mx/robots.txt", "https://heraldodemexico.com.mx/noticias/buscar/?buscar=migraci%C3%B3n+venezolana")
 print("HC:")
 chekear_robots("https://www.elheraldo.co/robots.txt", "https://www.elheraldo.co/buscador/?query=migraci%C3%B3n%20venezolana")
+print("Semana:")
+chekear_robots("https://www.semana.com/robots.txt", "https://www.semana.com")
+
+chekear_robots("https://archiveofourown.org/robots.txt", "https://archiveofourown.org")
 
