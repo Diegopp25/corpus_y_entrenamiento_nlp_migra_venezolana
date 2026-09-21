ae1c0a33fc55cb6942ca61c941f0542d9215655b
Diego <diegoseptiembre991@gmail.com>
2026-09-21 07:15:24 -0500

    Refactorización: la información de main.py y nlp_preprocesing.ipynb la traslade al main notebook para mejorar legibilidad y para que se ajuste al modelo ideal de la arquitectura ELT, siendo main notebook el modulo principal de la capa de transformación.

--- main.py
+++
@@ -1,5 +0,0 @@
-from extraction_codes.full_extraction import extraer_data_completa    
-
-#El parametro de la función indíca sí queremos extraer todas las noticas posibles o solo un número determinado
-
-extraer_data_completa(None)
--- to_labelbox.py
+++ to_labelbox.py
@@ -1,54 +1,56 @@
 import pandas as pd
 import labelbox as lb
 
-# =====================================
-# CONFIGURACIÓN
-# =====================================
+def enviar_a_labelbox():
 
-API_KEY = "lbx_zQcvCsJmScIYkgTlacTLBCeRlS4Aam1PXfWquIdrweI"
+    # =====================================
+    # CONFIGURACIÓN
+    # =====================================
 
-CSV_PATH = "Data/dataset_migracion_venezolana_limpio_estandarizado.csv"
+    API_KEY = "lbx_zQcvCsJmScIYkgTlacTLBCeRlS4Aam1PXfWquIdrweI"
 
-# =====================================
-# CONECTAR A LABELBOX
-# =====================================
+    CSV_PATH = "Data/dataset_migracion_venezolana_limpio_estandarizado.csv"
 
-client = lb.Client(API_KEY)
+    # =====================================
+    # CONECTAR A LABELBOX
+    # =====================================
 
-# Crear dataset
-dataset = client.create_dataset(
-    name="Noticias_Migracion"
-)
+    client = lb.Client(API_KEY)
 
-# =====================================
-# LEER CSV
-# =====================================
-
-df = pd.read_csv(CSV_PATH)
-
-
-# =====================================
-# CONSTRUIR DATA ROWS
-# =====================================
-
-data_rows = []
-
-for _, row in df.iterrows():
-
-    data_rows.append(
-        {
-            "row_data": row["Cuerpo"],
-            "global_key": row["ID"]
-        }
+    # Crear dataset
+    dataset = client.create_dataset(
+        name="Noticias_Migracion"
     )
 
-# =====================================
-# SUBIR A LABELBOX
-# =====================================
+    # =====================================
+    # LEER CSV
+    # =====================================
 
-task = dataset.create_data_rows(data_rows)
+    df = pd.read_csv(CSV_PATH)
 
-task.wait_till_done()
 
-print("Dataset cargado correctamente")
-print(f"Total registros: {len(data_rows)}")
+    # =====================================
+    # CONSTRUIR DATA ROWS
+    # =====================================
+
+    data_rows = []
+
+    for _, row in df.iterrows():
+
+        data_rows.append(
+            {
+                "row_data": row["Cuerpo"],
+                "global_key": row["ID"]
+            }
+        )
+
+    # =====================================
+    # SUBIR A LABELBOX
+    # =====================================
+
+    task = dataset.create_data_rows(data_rows)
+
+    task.wait_till_done()
+
+    print("Dataset cargado correctamente")
+    print(f"Total registros: {len(data_rows)}")
